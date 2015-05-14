d485e7e79f6d36f0e87f751a2b8998042cf6bf74
JackRFuller <jackrfuller@outlook.com>
2015-05-14 13:32:48 +0100

    Initial Commit

---
+++ Transform_Unity/Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAction : MonoBehaviour {
+
+	[SerializeField] float RaycastRange;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonUp(0))
+		{
+			ScaleObject();
+		}
+
+		if(Input.GetMouseButton(1))
+		{
+			ShrinkObject();
+		}
+	
+	}
+
+	void ScaleObject()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 100))
+		{
+			Debug.Log(hit.collider.name);
+			if(hit.collider.tag == "Scalable")
+			{
+				hit.collider.gameObject.GetComponent<ScaleObject>().IncreaseObjectSize();
+				Debug.Log("Hit");
+			}
+		}
+			
+	}
+
+	void ShrinkObject()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, RaycastRange))
+		{
+			if(hit.collider.tag == "Player")
+			{
+				Debug.Log("Success1");
+			}
+		}
+	}
+}
---
+++ Transform_Unity/Assets/Scripts/ScaleObject.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleObject : MonoBehaviour {
+
+	public enum Orientation
+	{
+		x,
+		y,
+		z,
+	};
+
+	[SerializeField] GameObject ParentObject;
+
+	public Orientation ObjectOrientation;
+	[SerializeField] float MaxSize;
+	[SerializeField] float MinSize;
+	[SerializeField] float ScaleRate;
+	[SerializeField] Material ScaledMaterial;
+
+	// Use this for initialization
+	void Start () {
+
+		ParentObject = transform.parent.root.gameObject;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void IncreaseObjectSize()
+	{
+		if (ObjectOrientation == Orientation.x)
+		{
+			if(ParentObject.transform.localScale.x <= MaxSize)
+			{
+
+			}
+		}
+		if (ObjectOrientation == Orientation.y)
+		{
+			
+		}
+		if (ObjectOrientation == Orientation.z)
+		{
+			if(ParentObject.transform.localScale.z <= MaxSize)
+			{
+				ScaleZ();
+			}
+			else
+			{
+				ParentObject.transform.localScale = new Vector3(ParentObject.transform.localScale.x,ParentObject.transform.localScale.y,MaxSize);
+			}
+		}
+	}
+
+	void ScaleZ()
+	{
+		ParentObject.transform.localScale = new Vector3(ParentObject.transform.localScale.x,ParentObject.transform.localScale.y,ParentObject.transform.localScale.z + ScaleRate);
+	}
+}
